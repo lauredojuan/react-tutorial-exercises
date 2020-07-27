3e44a8f7edb6c2c5129e6f8243a466d0bcd8a08d
Juan L <lauredojuan@gmail.com>
2020-07-27 16:43:15 +0000

    ex2.2

--- exercises/02-maping-array-to-li/index.js
+++ exercises/02-maping-array-to-li/index.js
@@ -3,11 +3,8 @@ import ReactDOM from "react-dom";
 
 const animals = ["Horse", "Turtle", "Elephant", "Monkey"];
 
-/**
- * change the content inside the map function
- **/
 const animalsInHTML = animals.map((singleAnimal, i) => {
-	return <li>hello</li>;
+	return <li>{singleAnimal}</li>;
 });
 
 ReactDOM.render(<ul>{animalsInHTML}</ul>, document.querySelector("#myDiv"));
